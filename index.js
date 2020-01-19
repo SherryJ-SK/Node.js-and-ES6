f7f8c919628256c63d22659eb622bc2327f31084
Sherry Jin <a-sherry@live.com>
2020-01-19 23:57:23 +1100

    Axios in progress

--- index.js
+++ index.js
@@ -7,52 +7,76 @@
 // function init() {
 
 // init();
-// const questions = [
-
-// ];
-
-// function writeToFile(fileName, data) {
-
-// }
-
-// function init() {
-
-// init();
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const html = require("generateHTML");
+const html = require("./generateHTML");
 
-inquirer.prompt([
-    {
-        type: "input",
-        name: "username",
-        message: "Enter your name: "
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Enter your GitHub username: "
-    },
-    {
-        type: "input",
-        name: "location",
-        message: "Enter your location: "
-    }
-]).then(function (data) {
-    // const filename = data.name.toLowerCase().split(" ").join("") + ".json";
+function userInfo() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "Enter your name: "
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "location",
+            message: "Enter your location: "
+        }
+    ])
+};
 
-    // fs.writeFile(filename, function (err) {
-    //     if (err) {
-    //         throw err
-    //     }
-    // })
+userInfo()
+    .then(function (data) {
 
-    const queryUrl = `https://api.github.com/users/${data.github}`;
+        axios
+            .get(`https://api.github.com/users/${data.github}`)
+            .then(function (res) {
+                const { avatar_id } = res.data;
+                const { location } = res.data;
+                const { url } = res.data;
+                const { public_repos } = res.data;
+                const { followers } = res.data;
+                const { following } = res.data;
+                const { starred_url } = res.data;
+                
+                returnInfo(data);
 
-    axios.get(queryUrl)
-        .then(function (res) {
-            console.log(res.data)
-        })
-})
+                //read information from generateHTML.js
+                appendFileAsync("index.html", html.createHtml + "\n").then(function () {
+                    readFileAsync("index.html", "utf8").then(function (data) {
+                        console.log("Saved");
+                        console.log(data);
+                    });
+                });
+
+            });
+    });
+
+function returnInfo(data) {
+    return `
+    <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+    <h1 class="display-4">Hi! My name is ${data.username}</h1>
+    <p class="lead">I am from ${location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${github}</li>
+      <li class="list-group-item">GitHub Link: ${url}</li>
+    </ul>
+    </div>
+    </div>
+    </body>
+    </html>
+    `
+}
+
+
